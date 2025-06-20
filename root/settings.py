e2bd145d19d28329b5efdc1995c149c75b3b1d3f
dilshod-py <absaitovdev@gmail>
2025-06-20 17:37:46 +0500

    esdfg

--- root/settings.py
+++ root/settings.py
@@ -134,7 +134,7 @@ REST_FRAMEWORK = {
 
 }
 SPECTACULAR_SETTINGS = {
-    'TITLE': 'Gayrat PDP Leaderboard',
+    'TITLE': 'p29 PDP Leaderboard',
     'DESCRIPTION': 'This project for pdp',
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
